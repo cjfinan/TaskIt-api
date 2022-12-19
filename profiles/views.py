c1504613bbf0492e118d9d8f6ea887460d270390
cjfinan <cjfinan98@gmail.com>
2022-12-19 13:34:02 +0000

    [ADD] Profile views and serializers

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProfileSerializer
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
