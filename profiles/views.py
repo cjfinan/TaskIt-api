6e43447b30043d1fc58c02d42ad916d0d526d3a3
cjfinan <cjfinan98@gmail.com>
2022-12-21 10:30:16 +0000

    [ADD] profile filters to profiles serializer and views

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,30 @@
+from django.db.models import Count
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from .serializers import ProfileSerializer
 from .models import Profile
 from taskit_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        tasks_count=Count('owner__task', distinct=True),
+        boards_count=Count('owner__board', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'tasks_count',
+        'boards_count',
+    ]
     serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        tasks_count=Count('owner__task', distinct=True),
+        boards_count=Count('owner__board', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
